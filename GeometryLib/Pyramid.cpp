09f19fb92b5e605be9d46e649a27681a6234ade8
linxi <linxi@fulongtech.cn>
2014-08-25 16:19:38 +0800

    解决方台反面剔除问题

--- GeometryLib/Pyramid.cpp
+++ GeometryLib/Pyramid.cpp
@@ -21,15 +21,15 @@ void Pyramid::subDraw()
 	osg::Vec3 zAxis = m_height / m_height.length();
 	osg::Vec3 topOrg = m_org + m_height + m_offset;
 
-	osg::Vec3 p1 = m_org - m_xAxis * m_bottomXLen / 2.0 - yAxis * m_bottomYLen / 2.0;
-	osg::Vec3 p2 = m_org + m_xAxis * m_bottomXLen / 2.0 - yAxis * m_bottomYLen / 2.0;
-	osg::Vec3 p3 = m_org + m_xAxis * m_bottomXLen / 2.0 + yAxis * m_bottomYLen / 2.0;
-	osg::Vec3 p4 = m_org - m_xAxis * m_bottomXLen / 2.0 + yAxis * m_bottomYLen / 2.0;
+	osg::Vec3 bp1 = m_org - m_xAxis * m_bottomXLen / 2.0 - yAxis * m_bottomYLen / 2.0;
+	osg::Vec3 bp2 = m_org + m_xAxis * m_bottomXLen / 2.0 - yAxis * m_bottomYLen / 2.0;
+	osg::Vec3 bp3 = m_org + m_xAxis * m_bottomXLen / 2.0 + yAxis * m_bottomYLen / 2.0;
+	osg::Vec3 bp4 = m_org - m_xAxis * m_bottomXLen / 2.0 + yAxis * m_bottomYLen / 2.0;
 
-	osg::Vec3 p5 = topOrg - m_xAxis * m_topXLen / 2.0 - yAxis * m_topYLen / 2.0;
-	osg::Vec3 p6 = topOrg + m_xAxis * m_topXLen / 2.0 - yAxis * m_topYLen / 2.0;
-	osg::Vec3 p7 = topOrg + m_xAxis * m_topXLen / 2.0 + yAxis * m_topYLen / 2.0;
-	osg::Vec3 p8 = topOrg - m_xAxis * m_topXLen / 2.0 + yAxis * m_topYLen / 2.0;
+	osg::Vec3 tp1 = topOrg - m_xAxis * m_topXLen / 2.0 - yAxis * m_topYLen / 2.0;
+	osg::Vec3 tp2 = topOrg + m_xAxis * m_topXLen / 2.0 - yAxis * m_topYLen / 2.0;
+	osg::Vec3 tp3 = topOrg + m_xAxis * m_topXLen / 2.0 + yAxis * m_topYLen / 2.0;
+	osg::Vec3 tp4 = topOrg - m_xAxis * m_topXLen / 2.0 + yAxis * m_topYLen / 2.0;
 
 	osg::ref_ptr<osg::Vec3Array> vertexArr = new osg::Vec3Array;
 	osg::ref_ptr<osg::Vec3Array> normalArr = new osg::Vec3Array;
@@ -39,54 +39,54 @@ void Pyramid::subDraw()
 	colArr->push_back(m_color);
 	setColorArray(colArr, osg::Array::BIND_OVERALL);
 
-	vertexArr->push_back(p1);
-	vertexArr->push_back(p2);
-	vertexArr->push_back(p3);
-	vertexArr->push_back(p4);
+	vertexArr->push_back(bp4);
+	vertexArr->push_back(bp3);
+	vertexArr->push_back(bp2);
+	vertexArr->push_back(bp1);
 	normalArr->push_back(-zAxis);
 	for (int i = 0; i < 3; ++i)
 		normalArr->push_back(normalArr->back());
 
-	vertexArr->push_back(p5);
-	vertexArr->push_back(p6);
-	vertexArr->push_back(p7);
-	vertexArr->push_back(p8);
+	vertexArr->push_back(tp1);
+	vertexArr->push_back(tp2);
+	vertexArr->push_back(tp3);
+	vertexArr->push_back(tp4);
 	normalArr->push_back(zAxis);
 	for (int i = 0; i < 3; ++i)
 		normalArr->push_back(normalArr->back());
 
-	vertexArr->push_back(p1);
-	vertexArr->push_back(p2);
-	vertexArr->push_back(p6);
-	vertexArr->push_back(p5);
-	normalArr->push_back((p6 - p1) ^ (p5 - p2));
+	vertexArr->push_back(bp1);
+	vertexArr->push_back(bp2);
+	vertexArr->push_back(tp2);
+	vertexArr->push_back(tp1);
+	normalArr->push_back((tp2 - bp1) ^ (tp1 - bp2));
 	normalArr->back().normalize();
 	for (int i = 0; i < 3; ++i)
 		normalArr->push_back(normalArr->back());
 
-	vertexArr->push_back(p2);
-	vertexArr->push_back(p3);
-	vertexArr->push_back(p7);
-	vertexArr->push_back(p6);
-	normalArr->push_back((p7 - p2) ^ (p6 - p3));
+	vertexArr->push_back(bp2);
+	vertexArr->push_back(bp3);
+	vertexArr->push_back(tp3);
+	vertexArr->push_back(tp2);
+	normalArr->push_back((tp3 - bp2) ^ (tp2 - bp3));
 	normalArr->back().normalize();
 	for (int i = 0; i < 3; ++i)
 		normalArr->push_back(normalArr->back());
 
-	vertexArr->push_back(p3);
-	vertexArr->push_back(p4);
-	vertexArr->push_back(p8);
-	vertexArr->push_back(p7);
-	normalArr->push_back((p8 - p3) ^ (p7 - p4));
+	vertexArr->push_back(bp3);
+	vertexArr->push_back(bp4);
+	vertexArr->push_back(tp4);
+	vertexArr->push_back(tp3);
+	normalArr->push_back((tp4 - bp3) ^ (tp3 - bp4));
 	normalArr->back().normalize();
 	for (int i = 0; i < 3; ++i)
 		normalArr->push_back(normalArr->back());
 
-	vertexArr->push_back(p4);
-	vertexArr->push_back(p1);
-	vertexArr->push_back(p5);
-	vertexArr->push_back(p8);
-	normalArr->push_back((p5 - p4) ^ (p8 - p1));
+	vertexArr->push_back(bp4);
+	vertexArr->push_back(bp1);
+	vertexArr->push_back(tp1);
+	vertexArr->push_back(tp4);
+	normalArr->push_back((tp1 - bp4) ^ (tp4 - bp1));
 	normalArr->back().normalize();
 	for (int i = 0; i < 3; ++i)
 		normalArr->push_back(normalArr->back());
